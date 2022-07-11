8037ef156daf010602c172eccb246f7df43dd014
ZWN2001 <2134356214@qq.com>
2022-07-11 10:44:28 +0800

    feat:可以查票

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import re
+import urllib.parse
 from time import sleep
 
 import requests
@@ -12,7 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 import aiohttp
 
-global url, browser, username, password, wait, proxy
+global browser, wait, tasks
 
 
 # 初始化
@@ -21,51 +22,40 @@ def init():
     option = Options()
     option.add_argument('--disable-blink-features=AutomationControlled')
     # 定义为全局变量，方便其他模块使用
-    global url, browser, username, password, wait, proxy
-    proxy = {'https': '127.0.0.1:7890'}
-    # 登录界面的url
-    url = 'https://kyfw.12306.cn/otn/resources/login.html'
+    global browser, wait
     # 实例化一个chrome浏览器
     browser = webdriver.Chrome(options=option)
-    # 用户名
-    username = '2134356214@qq.com'
-    # 密码
-    password = '----'
     # 设置等待超时
     wait = WebDriverWait(browser, 20)
 
 
-# 登录
-def login():
-    # 打开登录页面
-    browser.get(url)
-    # 获取用户名输入框
-    user = wait.until(EC.presence_of_element_located((By.ID, 'J-userName')))
-    # 获取密码输入框
-    passwd = wait.until(EC.presence_of_element_located((By.ID, 'J-password')))
-    # 输入用户名
-    user.send_keys(username)
-    # 输入密码
-    passwd.send_keys(password)
-
-    browser.find_element(By.ID, "J-login").click()
-
-
-# 模拟拖动
-def move_to_gap():
-    # 得到滑块标签
-    slider = wait.until(EC.presence_of_element_located((By.ID, 'nc_1_n1z')))
-    ActionChains(browser).drag_and_drop_by_offset(slider, 300, 0).perform()
-
-
-def get_cookie():
+# 拿到的cookie只是一部分
+def get_part_cookie():
+    browser.get("https://kyfw.12306.cn/otn/leftTicket/init?linktypeid=dc")
+    sleep(1)
+    browser.find_element(By.ID, "sf2").click()
     c = browser.get_cookies()
-    cookies = {}
+    cookies = ''
     # 获取cookie中的name和value,转化成requests可以使用的形式
     for cookie in c:
-        cookies[cookie['name']] = cookie['value']
+        cookies += "{}={};".format(cookie['name'], cookie['value'])
 
-    print(cookies)
+    return cookies
+
+
+def get_another_cookie(stationName, date, from_station, to_station):
+    from_station_code = stationName[from_station]
+    to_station_code = stationName[to_station]
+    from_station_str = from_station + "," + from_station_code
+    to_station_str = to_station + "," + to_station_code
+    from_station_escape = escape(from_station_str)
+    to_station_escape = escape(to_station_str)
+    another_cookie = ("_jc_save_fromStation={};"
+                      "_jc_save_toStation={};"
+                      "_jc_save_fromDate={};"
+                      " _jc_save_toDate={};"
+                      "_jc_save_wfdc_flag=dc").format(from_station_escape, to_station_escape, date, date)
+    return another_cookie
 
 
 def get_all_station_name_and_code():
@@ -78,14 +68,13 @@ def get_all_station_name_and_code():
     return stationName, stationCode
 
 
-def get_query_url(text, date, from_station, to_station):
-    # 构建用于查询列车车次信息的url
-    # 参数：日期，出发地，到达地
+# 构建用于查询列车车次信息的url
+def get_query_url(stationName, date, from_station, to_station):
     # key为车站名称， value为车站代号
 
     date = date
-    from_station = text[from_station]
-    to_station = text[to_station]
+    from_station = stationName[from_station]
+    to_station = stationName[to_station]
     print([from_station, to_station])
     # 构造直接获取车次信息的url
     query_url = ("https://kyfw.12306.cn/otn/leftTicket/query?leftTicketDTO.train_date={}"
@@ -97,26 +86,22 @@ def get_query_url(text, date, from_station, to_station):
     return query_url
 
 
-async def get_info_from_query_url(query_url):
-    cookie = "tk=YdFLdVKmaySrnWBxT8QCfZ6RA5Vf5k-rbcS1S0; JSESSIONID=8082F95D9CBF715F28E0AFC924BB051F; " \
-             "RAIL_EXPIRATION=1657682972442; " \
-             "RAIL_DEVICEID" \
-             "=e8EMu5K2CHTHt8PLHUia5L5Ibu_lWeVTFaIitxQ1UkZ4eDZgX81p0Li5JVD47mYGJVHxU69VVUz1TnLdd2pL3imK2IcLnGDrYpw02" \
-             "-Xjl_jrJaJ6sMqkg4Fp2EaearN9UgJbAWPH2ey1xUlH9qoJCNyafVPSOVIo; guidesStatus=off; " \
-             "highContrastMode=defaltMode; cursorStatus=off; BIGipServerpool_passport=199492106.50215.0000; " \
-             "route=c5c62a339e7744272a54643b3be5bf64; BIGipServerpassport=870842634.50215.0000; " \
-             "current_captcha_type=Z; _jc_save_toStation=%u5317%u4EAC%2CBJP; " \
-             "fo=lychk6583r3ve8grL4FA6lRnmc8pYiQJMzoIogEpjAN" \
-             "+Wvf8KuYinWwvkuxL4RsfXlPqgmgO9tBrFErdJPOC9Cs6fybJyq58xTqFtFPdq4FBlLYXuWlpHqhAb5a8HF14gT9cv9l/VUxwkU8dH" \
-             "/32DQ9ngCzjAo/RBUAbfKByRlUVXbhfH2hZKbtse9c%3D; _jc_save_fromDate=2022-07-14; _jc_save_wfdc_flag=dc; " \
-             "_jc_save_toDate=2022-07-10; _jc_save_fromStation=%u4E0A%u6D77%2CSHH; " \
-             "BIGipServerportal=2949906698.17695.0000; BIGipServerotn=568852746.24610.0000; " \
-             "uKey=8b53fb39e2c3d400819319fc95e08e017b875d866168a46db8e3dc78eb3a32c8 "
+def escape(data):
+    fill = re.findall('[\u4e00-\u9fa5]', data)
+    if len(fill) > 0:
+        for ch in fill:
+            data = data.replace(ch, '5Cu{0}'.format(hex(ord(ch))[2:].upper()))
+    return urllib.parse.quote(data.encode('unicode-escape'), safe='*@-_+./').replace('5Cu', '%u')
+
+
+async def get_info_from_query_url(query_url, stationName, date, from_station, to_station):
+    cookie1 = get_part_cookie()
+    cookie2 = get_another_cookie(stationName, date, from_station, to_station)
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/61.0.3163.100 Safari/537.36",
-        "Cookie": cookie
+        "Cookie": cookie1 + cookie2
     }
     async with aiohttp.ClientSession() as session:
         async with session.get(query_url, headers=headers) as resp:
@@ -142,30 +127,30 @@ async def get_info_from_query_url(query_url):
                 print(train_number, from_station_code, from_station_name, to_station_code, to_station_name, go_time,
                       arrive_time, cost_time, special_class_seat, first_class_seat, second_class_seat, soft_sleep,
                       hard_sleep, hard_seat, no_seat)
-                # TODO:写入数据库或文件
+    # #             # TODO:写入数据库或文件
 
 
 # 主程序
 async def main():
     # 初始化
+    global tasks
     init()
-    # 登录
-    login()
-    sleep(2)
-    # 移动滑块
-    move_to_gap()
-    sleep(2)
 
-    # stationName, stationCode = get_all_station_name_and_code()
-    # query_url = get_query_url(stationName, '2022-07-14', '上海', '北京')
-    # tasks = [asyncio.create_task(get_info_from_query_url(query_url))]
-    # await asyncio.wait(tasks)
-    # get_info_from_query_url(query_url)
+    stationName, stationCode = get_all_station_name_and_code()
+    query_url = get_query_url(stationName, '2022-07-14', '上海', '北京')
+    tasks = [asyncio.create_task(get_info_from_query_url(query_url, stationName, '2022-07-14', '上海', '北京'))]
+    # for from_station in stationName:
+    #     for to_station in stationName:
+    #         if from_station != to_station:
+    #             for i in range(1, 30):
+    #                 query_url = get_query_url(stationName, '2022-07-14', from_station, to_station)
+    #                 tasks = [asyncio.create_task(get_info_from_query_url(
+    #                     query_url, stationName, '2022-07-14', from_station, to_station))]
+
+    await asyncio.wait(tasks)
 
 
 # 程序入口
 if __name__ == '__main__':
-    # main()
-    # asyncio.run(main())
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
