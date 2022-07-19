68561b0c9cdcc07cdb07e1f5d35ffba27f8e7af8
ZWN2001 <2134356214@qq.com>
2022-07-19 09:04:53 +0800

    feat:数据库基本配置

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import asyncio
 import re
 import urllib.parse
+
+import pymysql
 import requests
 import aiohttp
 
-global browser, wait, tasks
+global browser, wait, tasks, db
 
 
 def get_another_cookie(stationName, date, from_station, to_station):
@@ -32,6 +34,24 @@ def get_all_station_name_and_code():
     return stationName, stationCode
 
 
+def write_all_station_name_and_code_to_db(stationName):
+    cursor = db.cursor()
+    for sn in stationName.keys():
+        city = sn
+        if sn[-1:] in ("东", "西", "南", "北"):
+            city = sn[0:-1]
+        sql = "INSERT INTO station VALUES (%s, %s, %s) "
+        cursor.execute(sql, (stationName[sn], sn, city))
+    try:
+        db.commit() # 所有sql一起提交以提高效率
+    except:
+        # 发生错误时回滚
+        db.rollback()
+
+    # 关闭数据库连接
+    db.close()
+
+
 # 构建用于查询列车车次信息的url
 def get_query_url(stationName, date, from_station, to_station):
     # key为车站名称， value为车站代号
@@ -47,7 +67,7 @@ def get_query_url(stationName, date, from_station, to_station):
 
 
 def escape(data):
-    fill = re.findall('[\u4e00-\u9fa5]', data)
+    fill = re.findall(r'[\u4e00-\u9fa5]', data)
     if len(fill) > 0:
         for ch in fill:
             data = data.replace(ch, '5Cu{0}'.format(hex(ord(ch))[2:].upper()))
@@ -56,7 +76,7 @@ def escape(data):
 
 async def get_info_from_query_url(query_url, stationName, date, from_station, to_station):
     # TODO：建议换用自己的cookie，否则不保证cookie过期而导致的问题，或者使用main2.py,自动获取token
-    cookie1 = "JSESSIONID=8E1758D7EC4330E6FE0A4F2A9BF39FBC; RAIL_EXPIRATION=1657682972442; RAIL_DEVICEID=e8EMu5K2CHTHt8PLHUia5L5Ibu_lWeVTFaIitxQ1UkZ4eDZgX81p0Li5JVD47mYGJVHxU69VVUz1TnLdd2pL3imK2IcLnGDrYpw02-Xjl_jrJaJ6sMqkg4Fp2EaearN9UgJbAWPH2ey1xUlH9qoJCNyafVPSOVIo; guidesStatus=off; highContrastMode=defaltMode; cursorStatus=off; BIGipServerpool_passport=199492106.50215.0000; route=c5c62a339e7744272a54643b3be5bf64; BIGipServerpassport=870842634.50215.0000; current_captcha_type=Z; fo=lychk6583r3ve8grL4FA6lRnmc8pYiQJMzoIogEpjAN+Wvf8KuYinWwvkuxL4RsfXlPqgmgO9tBrFErdJPOC9Cs6fybJyq58xTqFtFPdq4FBlLYXuWlpHqhAb5a8HF14gT9cv9l/VUxwkU8dH/32DQ9ngCzjAo/RBUAbfKByRlUVXbhfH2hZKbtse9c%3D; BIGipServerportal=2949906698.17695.0000;BIGipServerotn=1926824202.24610.0000"
+    cookie1 = "JSESSIONID=6C15081C8C2298D4003F1A17806428A0; tk=E2zTT5p5Cfg7Uha0fWmX-sj1Ik4kYzsg27S1S0; guidesStatus=off; highContrastMode=defaltMode; cursorStatus=off; fo=lychk6583r3ve8grL4FA6lRnmc8pYiQJMzoIogEpjAN+Wvf8KuYinWwvkuxL4RsfXlPqgmgO9tBrFErdJPOC9Cs6fybJyq58xTqFtFPdq4FBlLYXuWlpHqhAb5a8HF14gT9cv9l/VUxwkU8dH/32DQ9ngCzjAo/RBUAbfKByRlUVXbhfH2hZKbtse9c%3D;  BIGipServerotn=1072693770.38945.0000; BIGipServerpool_passport=283378186.50215.0000; RAIL_EXPIRATION=1658141541162; RAIL_DEVICEID=imvIBxu2tpyGT6uOFVNQ_7V0aWLZNkjJ8p2mdZQkq2kkKhHCX7KQo4ZUuDP-j8lEMK754pWISeD6bLke46xY33I4M8KCSCOOmfwWy_-LGSQBHUeHMqXEHv_ZvZOiUm9vTh3Z30IcfAVChEVKm21F6_36N5HRFQna; route=9036359bb8a8a461c164a04f8f50b252; uKey=476a47c3fdd124b23cf1378176af7c94ebbe7823bd6897fded2e78a14b54bab3; current_captcha_type=Z;"
     cookie2 = get_another_cookie(stationName, date, from_station, to_station)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -74,7 +94,7 @@ async def get_info_from_query_url(query_url, stationName, date, from_station, to
                 from_station_name = from_station  # 出发站名称
                 to_station_code = data_list[7]  # 到达站代号
                 to_station_name = to_station  # 到达站名称
-                go_time = data_list[8]   # 出发时间
+                go_time = data_list[8]  # 出发时间
                 arrive_time = data_list[9]  # 到达时间
                 cost_time = data_list[10]  # 历时
                 special_class_seat = data_list[32] or '--'  # 商务/特等座
@@ -84,9 +104,9 @@ async def get_info_from_query_url(query_url, stationName, date, from_station, to
                 hard_sleep = data_list[28] or '--'  # 硬卧
                 hard_seat = data_list[29] or '--'  # 硬座
                 no_seat = data_list[26] or '--'  # 无座
-                print(train_number, from_station_code, from_station_name, to_station_code, to_station_name, go_time,
-                      arrive_time, cost_time, special_class_seat, first_class_seat, second_class_seat, soft_sleep,
-                      hard_sleep, hard_seat, no_seat)
+                # print(train_number, from_station_code, from_station_name, to_station_code, to_station_name, go_time,
+                #       arrive_time, cost_time, special_class_seat, first_class_seat, second_class_seat, soft_sleep,
+                #       hard_sleep, hard_seat, no_seat)
     #             # TODO:写入数据库或文件
 
 
@@ -96,12 +116,13 @@ async def main():
     global tasks
 
     stationName, stationCode = get_all_station_name_and_code()
-    query_url = get_query_url(stationName, '2022-07-14', '上海', '北京')
-    tasks = [asyncio.create_task(get_info_from_query_url(query_url, stationName, '2022-07-14', '上海', '北京'))]
+    write_all_station_name_and_code_to_db(stationName)
+    query_url = get_query_url(stationName, '2022-07-21', '上海', '北京')
+    tasks = [asyncio.create_task(get_info_from_query_url(query_url, stationName, '2022-07-21', '上海', '北京'))]
     # TODO：循环，自己改即可
-
-    # for from_station in stationName:
-    #     for to_station in stationName:
+    #
+    # for from_station in stationName.keys():
+    #     for to_station in stationName.keys():
     #         if from_station != to_station:
     #             for i in range(1, 30):
     #                 query_url = get_query_url(stationName, '2022-07-14', from_station, to_station)
@@ -113,5 +134,9 @@ async def main():
 
 # 程序入口
 if __name__ == '__main__':
+    db = pymysql.connect(host='localhost',
+                         user='root',
+                         password='-',
+                         database='-')#TODO:你的数据库信息
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
